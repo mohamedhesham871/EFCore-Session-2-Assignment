dda4c91d4f1cf222d5a3a8595cf63e2692049e21
20100 <mmmelkady23@gmail.com>
2025-02-26 21:08:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCore_Session_2_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
